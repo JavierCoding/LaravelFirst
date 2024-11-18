d1720dbf9d4168a9f2bb7b24b16effd1ce3bd1b9
Javier Bravo <javier.bravogu@elorrieta-errekamari.com>
2024-11-18 20:18:14 +0100

    añadida ruta

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,11 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        
+        $posts = Post::orderBy('created_at')->get();
+        return view('posts.index',['posts' => $posts]);
+
+
     }
 
     /**
@@ -36,7 +40,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return view('posts.show',['post'=>$post]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\PostController;
+Route::controller(PostController::class)->group(function () {
+    Route::get('/posts', 'index')->name('posts.index');
+    Route::get('/posts/{post}', 'show')->name('posts.show');
+});
+
