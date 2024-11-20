8a09ce70714530f19d69919f17702a2a50b3d409
Javier Bravo <javier.bravogu@elorrieta-errekamari.com>
2024-11-20 15:32:35 +0100

    >Añadidos Sofdelte en post, y creata vista index en views/posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+//added to allow soft deletes
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
 class Post extends Model
 {
-    //
+    use SoftDeletes;
+
 }
