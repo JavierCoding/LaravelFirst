cd945607b667b5463e5f51d50f10f0f629f75316
Javier Bravo <javier.bravogu@elorrieta-errekamari.com>
2024-11-21 18:50:51 +0100

    edit button, delete button Edit button doesnt really work as expected, its not updating buyt ADDING A NEW POST, gotta check it out a

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -83,6 +83,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('posts.index');
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,12 +5,24 @@
 <ul>
   {{--esto es un comentario: recorremos el listado de posts--}}
   @foreach ($posts as $post)
-    {{-- visualizamos los atributos del objeto --}}
-    <li>
+  {{-- visualizamos los atributos del objeto --}}
+  <li class="pt-1">
+    <div class="d-flex flex-row">
       <a href="{{route('posts.show',$post)}}"> {{$post->titulo}}</a>.
       Escrito el {{$post->created_at}}
-    </li>
-  @endforeach
+      <a class="btn btn-warning btn-sm" href="{{route('posts.edit',$post)}}"
+        role="button">Editar</a>
+
+      <form action="{{route('posts.destroy',$post)}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button class="btn btn-sm btn-danger" type="submit"
+          onclick="return confirm('Are you sure?')">Delete
+        </button>
+      </form>
+    </div>
+  </li>
+@endforeach
 </ul>
 
 @endsectionphp
